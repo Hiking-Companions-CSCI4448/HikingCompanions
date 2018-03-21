7bb1af52e88e9ef9461433c4fed648a66f76d40b
LeQin <frankqin1013@gmail.com>
2018-03-21 14:02:05 -0600

    add DataService template, Account class

--- HikingCompanions/src/app/app.module.ts
+++ HikingCompanions/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
